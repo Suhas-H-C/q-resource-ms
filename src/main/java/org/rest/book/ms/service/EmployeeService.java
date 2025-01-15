86334c315ba2376728e1faf4d4ecc9a13e3ce782
Suhas H C <suhashc193@gmail.com>
2025-01-15 20:59:16 +0530

    Added additional graphql calls to employee

---
+++ src/main/java/org/rest/book/ms/service/EmployeeService.java
@@ -0,0 +1,61 @@
+package org.rest.book.ms.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+import org.jboss.logging.Logger;
+import org.rest.book.ms.model.Employee;
+import org.rest.book.ms.port.GreetPort;
+
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
+@ApplicationScoped
+public class EmployeeService {
+
+    private final Logger log;
+    private final GreetPort port;
+
+    @Inject
+    public EmployeeService(Logger log, GreetPort port) {
+        this.log = log;
+        this.port = port;
+    }
+
+    public List<Employee> allEmployees() {
+        return getAllEmployees();
+    }
+
+    public Employee getEmployeeById(Integer id) {
+        return getById(id);
+    }
+
+    @Timeout(3000)
+    @Retry(maxRetries = 2, delay = 1000)
+    @Fallback(fallbackMethod = "fallBackEmployeeById")
+    public Employee getById(Integer id) {
+        log.info("Calling greet-ms for employee by Id " + id);
+        return port.employeeById(id);
+    }
+
+    @Timeout(3000)
+    @Retry(maxRetries = 2, delay = 1000)
+    @Fallback(fallbackMethod = "fallBackEmployees")
+    public List<Employee> getAllEmployees() {
+        log.info("Calling greet-ms for employees");
+        return port.allEmployees();
+    }
+
+    public List<Employee> fallBackEmployees() {
+        log.warn("Fallback method called for employees as greet-ms is not available/failed");
+        return emptyList();
+    }
+
+    public Employee fallBackEmployeeById(Integer id) {
+        log.warn("Fallback method called for employee as greet-ms is not available/failed with Id " + id);
+        return null;
+    }
+}
